d3c4e64c046f45881e30249c4916e5b3c206d2ca
Moodyean <andrefnmatos@gmail.com>
2024-05-06 19:40:21 +0100

    Frontend subproject: now at a new commit with a "dirty" state
    This diff indicates that the frontend subproject has been updated to a new commit, with the notation "-dirty" added to the commit hash. This suggests that

--- src/components/Testimonials/Testimonials.jsx
+++ src/components/Testimonials/Testimonials.jsx
@@ -1,12 +1,98 @@
-import React from 'react'
+import React, { useState, useEffect, useRef } from 'react';
+import { HiChevronLeft, HiChevronRight } from 'react-icons/hi';
+import { motion } from 'framer-motion';
+import { images } from '../../constants';
+import Testimonials_data from '../../components/Testimonials_data';
 import './Testimonials.scss';
 
 const Testimonials = () => {
-  return (
-    <div id='Testimonials'>
-      <h1>Testimonials</h1>
-    </div>
-  )
-}
+  const [currentIndex, setCurrentIndex] = useState(0);
 
-export default Testimonials
+  const handleClick = (index) => {
+    setCurrentIndex(index);
+  };
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      handleClick(currentIndex === Testimonials_data.length - 1 ? 0 : currentIndex + 1);
+    }, 10000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, [currentIndex]);
+
+  const NavigationDots = () => {
+    return (
+      <div className="app__testimonials-dots">
+        {Testimonials_data.map((_, index) => (
+          <div
+            key={index}
+            className={`app__testimonials-dot ${currentIndex === index ? 'active' : ''}`}
+            onClick={() => handleClick(index)}
+          />
+        ))}
+      </div>
+    );
+  };
+
+  return (
+    <div className='app__testimonials app__flex'>
+      <div className='app__master-container'>
+        <div className='app__testimonials-info'>
+          <div className='app__testimonials-title'>
+            <motion.div
+              whileInView={{ x: [-100, 0], opacity: [0, 1] }}
+              transition={{ duration: 1 }}
+              className='app__testimonials-title-title'>&lt;testimonials/&gt;
+            </motion.div>
+            <motion.div
+              whileInView={{ x: [-100, 0], opacity: [0, 1] }}
+              transition={{ duration: 1.5 }}
+              className='app__testimonials-title-info'>words of clients, collaborators and organizations who I have helped to empower and reach their fullest potential.
+              <div className='app__Skills-blink'>|</div>
+            </motion.div>
+          </div>
+
+          <div className='app__testimonials-info-content'>
+            <div className='app__testimonials-box'>
+              <div className="app__testimonials-btn-left"
+                onClick={() => handleClick(currentIndex === 0 ? Testimonials_data.length - 1 : currentIndex - 1)}>
+                <HiChevronLeft />
+              </div>
+
+              <div className="app__testimonials-item">
+                <img src={Testimonials_data[currentIndex].image} alt={Testimonials_data[currentIndex].name} />
+                <div className="app__testimonials-content">
+                  <div className="app__testimonials-content-text">{Testimonials_data[currentIndex].message}</div>
+
+                  <div className='app__testimonials-content-titles'>
+                    <div className="app__testimonials-content-titles-name">{Testimonials_data[currentIndex].name}</div>
+                    <div className="app__testimonials-content-titles-job">{Testimonials_data[currentIndex].title}</div>
+                  </div>
+                </div>
+              </div>
+
+              <div className="app__testimonials-btn-right" onClick={() => handleClick(currentIndex === Testimonials_data.length - 1 ? 0 : currentIndex + 1)}>
+                <HiChevronRight />
+              </div>
+
+            </div>
+            <div className='app__testimonials-nav'>
+              <div className="app__testimonials-btn-left2"
+                onClick={() => handleClick(currentIndex === 0 ? Testimonials_data.length - 1 : currentIndex - 1)}>
+                <HiChevronLeft />
+              </div>
+              <NavigationDots />
+              <div className="app__testimonials-btn-right2" onClick={() => handleClick(currentIndex === Testimonials_data.length - 1 ? 0 : currentIndex + 1)}>
+                <HiChevronRight />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Testimonials;
