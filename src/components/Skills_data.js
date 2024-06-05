8aedbd821b2d06485b04e5fbbd5d14e9e973c23f
Moodyean <andrefnmatos@gmail.com>
2024-06-05 18:12:42 +0100

    skills change

---
+++ src/components/Skills_data.js
@@ -0,0 +1,65 @@
+import { images } from '../constants';
+
+const Skills_data = [
+    {
+        id: 1,
+        image: `${images.javascript}`,
+        name: "Javascript",
+    },
+    {
+        id: 2,
+        image: `${images.html}`,
+        name: "Html",
+    },
+    {
+        id: 3,
+        image: `${images.css}`,
+        name: "Css",
+    },
+    {
+        id: 4,
+        image: `${images.react}`,
+        name: "React",
+    },
+    {
+        id: 5,
+        image: `${images.sass}`,
+        name: "Sass",
+    },
+    {
+        id: 6,
+        image: `${images.node}`,
+        name: "Node Js",
+    },
+    {
+        id: 7,
+        image: `${images.redux}`,
+        name: "Redux",
+    },
+    {
+        id: 8,
+        image: `${images.git}`,
+        name: "Git",
+    },
+    {
+        id: 9,
+        image: `${images.figma}`,
+        name: "Figma",
+    },
+    {
+        id: 10,
+        image: `${images.photoshop}`,
+        name: "Photoshop",
+    },
+    {
+        id: 11,
+        image: `${images.illustrator}`,
+        name: "Illustrator",
+    },
+    {
+        id: 12,
+        image: `${images.aftereffects}`,
+        name: "After Effects",
+    },
+]
+export default Skills_data;
